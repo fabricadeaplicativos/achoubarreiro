7e74acb3bd2b415dbcf96ecc1dbb397a9724ec50
Renan Dias <renansdiaz@gmail.com>
2014-11-27 15:48:51 -0200

    Debugging

--- www/js/app.js
+++ www/js/app.js
@@ -7,9 +7,9 @@ function myCustomOnNotificationHandler(res) {
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'achouBarreiro' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('achouBarreiro', ['ionic', 'achouBarreiro.controllers'])
+angular.module('achouBarreiro', ['ionic', 'achouBarreiro.controllers', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaPush, $http) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -27,7 +27,7 @@ angular.module('achouBarreiro', ['ionic', 'achouBarreiro.controllers'])
 
     // Make http available globaly
     window.tokenRegister = function(token) {
-
+      alert("http://achoubarreiro.goldarkapi.com/push/devices?token=" + token);
       $http.get("http://achoubarreiro.goldarkapi.com/push/devices?token=" + token, {
           headers: {
               "Accept": "application/json",
